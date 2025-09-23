332a69712fe6728616a97a0c4e5d1008629223af
Tasnim Jabir <tasnimjabircse@gmail.com>
2025-09-23 20:45:51 +0100

    Sync local files with remote repo

---
+++ FindButterfly/ButterflyClassification/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ButterflyclassificationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ButterflyClassification'
